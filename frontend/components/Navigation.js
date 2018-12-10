6f96ab3c4a24b986f592b39bd5b415910c85cd28
TomNagle <tomanagle@gmail.com>
2018-12-10 12:51:36 +0000

    Add register to the navigation

--- frontend/components/Navigation.js
+++ frontend/components/Navigation.js
@@ -1,69 +1,63 @@
-import React, { Component, Fragment } from 'react'
-import Link from 'next/link'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import AccountCircle from '@material-ui/icons/AccountCircle'
-import Button from '@material-ui/core/Button'
-import MenuItem from '@material-ui/core/MenuItem'
-import Menu from '@material-ui/core/Menu'
-import { compose, graphql } from 'react-apollo'
-import withData from '../lib/withData'
-import gql from 'graphql-tag'
+import React, { Component, Fragment } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Link from 'next/link';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class Navigation extends Component {
-
-  constructor (props) {
-    super(props)
-
+  constructor(props) {
+    super(props);
   }
 
   handleMenu = event => {
-    this.setState({anchorEl: event.currentTarget})
-  }
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
   handleClose = () => {
-    this.setState({anchorEl: null})
-  }
+    this.setState({ anchorEl: null });
+  };
 
-  render () {
-    const {Me, title} = this.props
+  render() {
+    const { Me, title } = this.props;
 
-    return (<Fragment>
-
-        <AppBar position="static" classes={{root: 'nav-container'}} style={{backgroundColor: '#fff'}}>
+    return (
+      <Fragment>
+        <AppBar
+          position="static"
+          classes={{ root: 'nav-container' }}
+          style={{ backgroundColor: '#fff' }}
+        >
           <Toolbar>
-
-            <Typography variant="title" color="inherit" style={{flexGrow: 1}}>
+            <Typography variant="title" color="inherit" style={{ flexGrow: 1 }}>
               {title}
             </Typography>
 
-            {Me ?
+            {Me ? (
               <p>Welcome {Me.firstName}</p>
-              : <Link
-                prefetch
-                href="/login"><Button>Login</Button></Link>
-
-            }
-
+            ) : (
+              <Fragment>
+                <Link prefetch href="/login">
+                  <Button>Login</Button>
+                </Link>
+                <Link prefetch href="/register">
+                  <Button>Register</Button>
+                </Link>
+              </Fragment>
+            )}
           </Toolbar>
         </AppBar>
 
-
         <style jsx>{`
-
-      .nav-container {
-        flex-grow: 1;
-        background-color: #fff !important;
-      }
-
-    `}</style>
+          .nav-container {
+            flex-grow: 1;
+            background-color: #fff !important;
+          }
+        `}</style>
       </Fragment>
-    )
+    );
   }
 }
 
-export default Navigation
-
+export default Navigation;
