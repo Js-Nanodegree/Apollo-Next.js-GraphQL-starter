271710bd93f71b1d6eb2fc980a54462d17cce3fc
Tom Nagle <t.nagle@procurementleaders.com>
2019-04-04 12:44:34 +0100

    Update morgan

--- server/app/graphql/resolvers/auth.mutations.ts
+++ server/app/graphql/resolvers/auth.mutations.ts
@@ -1,11 +1,6 @@
-import Controller from "../../controllers/auth.controller";
-import { IContext } from "../../types/generic";
+import Controller from '../../controllers/auth.controller';
+import { IContext } from '../../types/generic';
 export default {
-  /* The subscribe function should be replaced with the invite function so it can be reused
-   for users inviting themselves and users inviting others from their company */
-  // Subscribe: (_: null, { input }: any) => {
-  //   return Controller.subscribe({ ...input });
-  // },
   Register: (_: null, { input }: any, context: IContext) => {
     return Controller.register({ ...input }, context);
   },
