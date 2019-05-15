74c3d99c36c8fbf72ea9657d8e9be5bd2e7d1bd1
Tom Nagle <t.nagle@procurementleaders.com>
2019-05-15 09:23:59 +0100

    Update app tests

--- frontend/components/__TESTS__/App.test.jsx
+++ frontend/components/__TESTS__/App.test.jsx
@@ -1,22 +1,59 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { CURRENT_USER_QUERY } from '../../constants/queries';
 import App from '../App';
+
+/*
+ * Next router can only be used on the client so it needs to be mocked
+ */
+jest.mock('next/router');
+
+const mocks = [
+  {
+    request: {
+      query: CURRENT_USER_QUERY
+    },
+    result: {
+      __typename: 'User',
+      _id: '123456',
+      email: 'test@example.com',
+      firstName: 'Test',
+      lastName: 'Example'
+    }
+  }
+];
 
 describe('App component', () => {
   it('should match snap shot', () => {
     const title = 'Test title';
 
-    const component = renderer.create(
-      <App title={title} showNavigation={false}>
-        <div>
-          <p>Test app children</p>
-        </div>
-      </App>
-    );
+    const withNavigation = renderer
+      .create(
+        <MockedProvider mocks={mocks}>
+          <App title={title} showNavigation>
+            <div>
+              <p>Test app children</p>
+            </div>
+          </App>
+        </MockedProvider>
+      )
+      .toJSON();
 
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(withNavigation).toMatchSnapshot();
+
+    const noNavigation = renderer
+      .create(
+        <App title={title} showNavigation={false}>
+          <div>
+            <p>Test app children</p>
+          </div>
+        </App>
+      )
+      .toJSON();
+
+    expect(noNavigation).toMatchSnapshot();
   });
 
   it('should render the page title', () => {
