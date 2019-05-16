3dd5fe65f4536f5f3ffa0c7865766fd2bfd63b9d
Tom Nagle <t.nagle@procurementleaders.com>
2019-05-16 08:45:07 +0100

    Fix env

--- frontend/config/env.js
+++ frontend/config/env.js
@@ -2,7 +2,7 @@
 const DEVELOPMENT_PATH = 'http://localhost:3000/graphql';
 
 // The path to your application in production. This should point to your GraphQL API
-const PRODUCTION_PATH = 'https://beta.pleventhub.com/graphql';
+const PRODUCTION_PATH = 'https://example.com/graphql';
 
 export const SERVER_PATH = 'http://localhost:8080/graphql';
 export const CLIENT_PATH = 'https://example.com/graphql';
@@ -16,7 +16,3 @@ export const IS_DEBUG = process.env.NODE_ENV !== 'production';
 export const GRAPHQL_ENDPOINT = IS_DEBUG ? DEVELOPMENT_PATH : PRODUCTION_PATH;
 
 export const WS_PATH = IS_DEBUG ? DEVELOPMENT_WS_PATH : PRODUCTION_WS_PATH;
-
-export const CLOUDINARY_NAME = 'matchandmeet';
-
-export const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_NAME}/upload`;
